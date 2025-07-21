82b921e00e361436b694e24322270b77104d9a23
Sravanthi Lakkaraju <lakkarajusravanthi@gmail.com>
2025-07-21 07:54:42 +0530

    Added Spring Boot base project under jobsphere folder

---
+++ jobsphere/src/test/java/com/jobsphere/JobsphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobsphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobsphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
